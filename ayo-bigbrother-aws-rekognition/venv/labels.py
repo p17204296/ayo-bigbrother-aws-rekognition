020e173d028d8eba7017c4099b34dffd593c8829
p17204296 <45291715+p17204296@users.noreply.github.com>
2022-04-14 16:03:50 +0100

    boto3 install & get Detect All labels from the image function in labels.py

---
+++ ayo-bigbrother-aws-rekognition/venv/labels.py
@@ -0,0 +1,17 @@
+import boto3
+from pprint import pprint
+import helper_img
+
+client = boto3.client('rekognition')
+
+#   image file
+img_url = 'https://p17204296.github.io/flexi_hires-front-end/images/videography@1x.jpg'
+
+#   Reference helper_img file
+img_bytes = helper_img.get_image_from_url(img_url)
+
+#   Detect All labels from the image
+result = client.detect_labels(Image={'Bytes': img_bytes})
+
+#   Print result
+pprint(result)
